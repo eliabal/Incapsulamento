6db3486e69e21004593695a1ae6066f998dc21c8
eliabal <eliabalinzo5@gmail.com>
2025-02-06 10:00:53 +0100

    Creato main.cpp
    Separato il main dal file "funzioni.cpp"

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <bitset>
+#include <sstream>
+
+#include "funzioni.h"
+#include "funzioni.cpp"
+
+using namespace std;
+
+int main(){
+    string testo = leggiFile(); // Creo 'testo' e ci metto il contenuto di 'messaggio.txt'
+    string binario = stringaABinario(testo); // Converto il contenuto di 'testo' in binario e lo salvo in 'binario'
+    cout << "Messaggio in binario: " << binario << endl; // Stampo binario a schermo
+    scriviFile(binario); // Scrivo 'binario' in 'frame.txt'
+
+    //system("pause");  Funziona solo su Windows
+
+    //testo = ""; // Pulisco 'testo'
+    //testo = leggiFile();
+    //testo = binarioAStringa(testo);
+    //cout << "Contenuto originale del file: " << testo << endl;
+    //system("pause");  Funziona solo su Windows
+}
