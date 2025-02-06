6db75df7012c4ed1d09bdd4955d56fc044e973ae
eliabal <eliabalinzo5@gmail.com>
2025-02-06 09:59:48 +0100

    Aggiornato funzioni.cpp
    Spostato il main dentro il nuovo file "main.cpp"

--- src/funzioni.cpp
+++ src/funzioni.cpp
@@ -73,18 +73,3 @@ string binarioAStringa(string input) {
     }
     return testo;
 }
-
-int main(){
-    string testo = leggiFile(); // Creo 'testo' e ci metto il contenuto di 'messaggio.txt'
-    string binario = stringaABinario(testo); // Converto il contenuto di 'testo' in binario e lo salvo in 'binario'
-    cout << "Messaggio in binario: " << binario << endl; // Stampo binario a schermo
-    scriviFile(binario); // Scrivo 'binario' in 'frame.txt'
-
-    //system("pause");  Funziona solo su Windows
-
-    //testo = ""; // Pulisco 'testo'
-    //testo = leggiFile();
-    //testo = binarioAStringa(testo);
-    //cout << "Contenuto originale del file: " << testo << endl;
-    //system("pause");  Funziona solo su Windows
-}
